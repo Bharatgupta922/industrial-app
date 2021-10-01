136bc13e8bc101756933390e4f07617915f0d0fa
Bharatgupta922 <gbharat922@gmail.com>
2021-10-01 17:26:34 +0530

    landing page updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
 import About from "./pages/About/About";
+import LandingPage from "./components/LandingPage";
+import {BrowserRouter as Router , Switch , Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
-		<>
-			<About />
-		</>
+		<Router>
+    <div className="app">
+        <Navbar />
+    <Switch>
+      <Route path = "/">
+        <LandingPage />
+      </Route>
+      <Route path = "/about">
+      <About />
+      </Route>
+        </Switch>
+        <Footer />
+    </div>
+	</Router>
 	);
 }
 
