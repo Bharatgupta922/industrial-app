627a83fe3c61b74c36abb6419621b837ec652935
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-10-01 18:01:15 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
 import "./App.css";
-import About from "./pages/About/About";
-import LandingPage from "./components/LandingPage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import ContactUs from "./pages/ContactUs/ContactUs";
 
 function App() {
 	return (
-		<Router>
-			<div className="app">
-				<Switch>
-					<Route path="/" exact>
-						<LandingPage />
-					</Route>
-					<Route path="/about">
-						<About />
-					</Route>
-				</Switch>
-				<Footer />
-			</div>
-		</Router>
+		<>
+			<ContactUs />
+		</>
 	);
 }
 
