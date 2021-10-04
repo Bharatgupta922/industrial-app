6a925eeba4361738fac84ff0bc582607b9c3514e
Bharatgupta922 <gbharat922@gmail.com>
2021-10-04 22:51:26 +0530

    explaination route added

---
+++ src/pages/OurProducts/Explaination.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Explaination = () => {
+    return (
+        <div>
+            <p>
+                hello 
+            </p>
+            <h1>shittt</h1>
+        </div>
+    )
+};
+
+export default Explaination;
